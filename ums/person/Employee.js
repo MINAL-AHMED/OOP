c8771fc6455191ad806674f8deec78d97746d5fc
Minal Ahmed <mimalahmed6219@gmail.com>
2024-01-28 18:51:37 +0600

    student and employee creating done

---
+++ ums/person/Employee.js
@@ -0,0 +1,28 @@
+const UniPerson = require("./UniPerson");
+
+const _employeeId = Symbol("employeeId");
+const _salary = Symbol("salary");
+
+class Employee extends UniPerson {
+  constructor(id, name, employeeId, salary) {
+    super(id, name);
+    this[_employeeId] = employeeId;
+    this[_salary] = salary;
+  }
+
+  get employeeId() {
+    return this[_employeeId];
+  }
+  get salary() {
+    return this[_salary];
+  }
+  set salary(value) {
+    this[_salary] = value;
+  }
+  toString() {
+    super.toString();
+    console.log(`EmployeeId = ${this[_employeeId]}, Salary = ${this[_salary]}`);
+  }
+}
+
+module.exports = Employee;
