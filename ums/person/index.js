b8915b4b75871c1da66e981d7d3629eb1a6a537a
Minal Ahmed <mimalahmed6219@gmail.com>
2024-01-30 18:04:08 +0600

    all class import and export

--- ums/person/index.js
+++ ums/person/index.js
@@ -3,6 +3,8 @@ const UniPerson = require("./UniPerson");
 const Guardian = require("./Guardian");
 const Student = require("./Student");
 const Employee = require("./Employee");
+const Teacher = require("./Teacher");
+const Stuff = require("./Stuff");
 
 module.exports = {
   Person,
@@ -10,4 +12,6 @@ module.exports = {
   Guardian,
   Student,
   Employee,
+  Teacher,
+  Stuff,
 };
